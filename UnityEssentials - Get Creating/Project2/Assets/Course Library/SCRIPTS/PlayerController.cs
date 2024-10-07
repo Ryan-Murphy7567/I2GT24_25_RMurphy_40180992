38942dc4e015e0f5ae291efd8cfa430cbcbfc60d
Ryan-Murphy7567 <ryanmurphy605@gmail.com>
2024-10-07 22:50:38 +0100

    unit2.2 complete
    added projectiles movement and destroy scripts

--- UnityEssentials - Get Creating/Project2/Assets/Course Library/SCRIPTS/PlayerController.cs
+++ UnityEssentials - Get Creating/Project2/Assets/Course Library/SCRIPTS/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     public float speed = 10.0f;
     public float horizontalInput;
     public float xRange = 10.0f;
-
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,6 +27,11 @@ public class PlayerController : MonoBehaviour
         if (transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // launch a projectile from the player
+            Instantiate(projectilePrefab,transform.position,projectilePrefab.transform.rotation);
         }
     }
 }
