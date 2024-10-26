a09be242646e4a163f97ef08fef238b6b03d03c6
Ryan-Murphy7567 <ryanmurphy605@gmail.com>
2024-10-26 16:15:57 +0100

    scripting for players and prefabs
    added some prefabs and beginning working on the combat system

---
+++ UnityEssentials - Get Creating/Knight quest (revamp)/Assets/scripts/enemyvalues.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyvalues : MonoBehaviour
+{
+    // enemy values
+
+    public string unitname;
+    public int maxhealth;
+    public int currenthealth;
+
+    public int attackdamage;
+
+}
---
+++ UnityEssentials - Get Creating/Knight quest (revamp)/Assets/scripts/playervalues.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    // setting out the core stats for the player
+
+    public string unitName; 
+
+    public int maxhealth;  
+    public int currenthealth;
+
+    public int attackdamage;
+}
