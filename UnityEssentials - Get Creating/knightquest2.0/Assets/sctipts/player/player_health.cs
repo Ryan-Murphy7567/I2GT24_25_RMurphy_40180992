8edd00b0531c8252c14f478f92bf0b9e7b43897f
Ryan-Murphy7567 <ryanmurphy605@gmail.com>
2024-10-27 14:15:50 +0000

    game over and death states
    game over and death states added as well as player health values

---
+++ UnityEssentials - Get Creating/knightquest2.0/Assets/sctipts/player/player_health.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Health : MonoBehaviour
+{
+    public int currentHealth = 0;
+    public int maxHealth = 100;
+
+    void Start()
+    { //health refreshes on combat start
+        currentHealth = maxHealth;
+    }
+    void Update()
+    { //creating a  test damage instance and Game over
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DamagePlayer(10);
+        }
+        if (currentHealth <= 0) { currentHealth = 0; }
+        if (currentHealth >= maxHealth) { currentHealth = 100; }
+        if (currentHealth <= 0) { death(); }
+        
+       
+    }
+    public void DamagePlayer(int damage)
+    {
+        currentHealth -= damage;
+
+    }
+    public void death() {
+    if (currentHealth <= 0) {SceneManager.LoadScene("Gameoverpanel"); }
+    
+    
+    
+    
+    }
+    
+  
+   
+}
