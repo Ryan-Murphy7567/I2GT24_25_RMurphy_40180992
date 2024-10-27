7ea71bffd70c6d17180c7bcccf4dcd0bfeda548c
Ryan-Murphy7567 <ryanmurphy605@gmail.com>
2024-10-27 23:46:59 +0000

    player movement fixed
    fixed player movement in open world

--- UnityEssentials - Get Creating/knightquest2.0/Assets/player_controller.cs
+++ UnityEssentials - Get Creating/knightquest2.0/Assets/player_controller.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class player_controller : MonoBehaviour
 { Rigidbody2D body;
-    float horizontal;
-    float vertical;
-    float moveLimiter = 0.7f;
+  
 
     public float speed=20.0f;
     // Start is called before the first frame update
@@ -17,18 +15,13 @@ public class player_controller : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        horizontal = Input.GetAxisRaw("horizontal"); //-1 is left
-        vertical = Input.GetAxisRaw("Vertical"); // -1 is down
+    { float horizontalInput= Input.GetAxis("Horizontal");
+ 
+      float verticalInput = Input.GetAxis("Vertical");
+        Vector3 direction= new Vector3(horizontalInput, verticalInput,0);
+        transform.Translate(direction*speed*Time.deltaTime);
     }
-    private void FixedUpdate()
-    {
-        if (horizontal != 0 && vertical != 0)
-        {
-            horizontal *= moveLimiter;
-            vertical *= moveLimiter;
-        }//check for diagonal movement
-        body.velocity = new Vector2(horizontal * speed, vertical * speed);
-    }
+   
+    
         
 }
