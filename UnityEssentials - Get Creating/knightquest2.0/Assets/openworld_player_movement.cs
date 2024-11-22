ce3baba8186fff20c70058bfa1e9e22601e1d23f
Ryan-Murphy7567 <ryanmurphy605@gmail.com>
2024-11-22 22:12:08 +0000

    camera tracking and basic inventory
    added camera tracking and basic UI for inventory as well as removed some non used files

--- UnityEssentials - Get Creating/knightquest2.0/Assets/openworld_player_movement.cs
+++ UnityEssentials - Get Creating/knightquest2.0/Assets/openworld_player_movement.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class openworld_player_movement : MonoBehaviour
 {
+    
+        Rigidbody2D body;
+
+    public float screenborder;
+
+    public float speed = 20.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal"); //movement-horizontal
+
+        float verticalInput = Input.GetAxis("Vertical"); //movement vertical
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+    public void OnCollisionEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("bounds"));
+        {
+            speed = 0.0f;
+        }
     }
 }
